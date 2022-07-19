4464876fde3ffa6bb2ea82b70fb50d24b7816b85
Evan Lou <Evanlou050102@gmail.com>
2022-07-19 12:31:49 -0700

    Added threading

--- main.py
+++ main.py
@@ -5,40 +5,39 @@ import matplotlib; matplotlib.use("TKAgg")
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-# Create figure for plotting
 import serial
+
+import threading
 
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
-xs = []
-y1s = []
-y2s = []
 
 
-
-
-def animate(i, xs, y1s, y2s):
-    # Read temperature (Celsius) from TMP102
+def openSerial():
+    global xs, y1s, y2s
+    xs = []
+    y1s = []
+    y2s = []
     ser = serial.Serial()
     ser.port = 'COM6'
     ser.baudrate = 9600
-    time.sleep(0.1)
+    time.sleep(2)
     ser.open()
+    while True:
+        pressure1 = int(ser.readline())
+        pressure2 = int(ser.readline())
+        # Add x and y to lists
+        xs.append(dt.datetime.now().strftime('%H:%M:%S.%f'))
+        y1s.append(pressure1)
+        print(y1s)
+        y2s.append(pressure2)
+        # Limit x and y lists to 20 items
+        xs = xs[-20:]
+        y1s = y1s[-20:]
+        y2s = y2s[-20:]
 
-    pressure1 = int(ser.readline())
-    pressure2 = int(ser.readline())
 
-    # Add x and y to lists
-    xs.append(dt.datetime.now().strftime('%H:%M:%S.%f'))
-    y1s.append(pressure1)
-    print(y1s)
-    y2s.append(pressure2)
-
-    # Limit x and y lists to 20 items
-    xs = xs[-20:]
-    y1s = y1s[-20:]
-    y2s = y2s[-20:]
-
+def animate(i):
     # Draw x and y lists
     ax.clear()
     ax.plot(xs, y1s)
@@ -52,5 +51,8 @@ def animate(i, xs, y1s, y2s):
 
 
 # Set up plot to call animate() function periodically
-ani = animation.FuncAnimation(fig, animate, fargs=(xs, y1s, y2s), interval=10)
+t = threading.Thread(target=openSerial, daemon=True)
+t.start()
+time.sleep(2)
+ani = animation.FuncAnimation(fig, animate, interval=10)
 plt.show()
